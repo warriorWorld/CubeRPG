2ede1886b044b5fc78c7dc011d574b52e700d86d
warriorWorld <772192594@qq.com>
2017-08-13 15:40:10 +0800

    camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,18 +6,20 @@ public class CameraController : MonoBehaviour {
 	public float PlayerCameraDistance{ get; set;}
 	public Transform cameraTarget;
 	Camera playerCamera;
-	float zoomSpeed=25f;
+	float zoomSpeed=35f;
 
 	// Use this for initialization
 	void Start () {
-		PlayerCameraDistance = 10f;
+		PlayerCameraDistance = 12f;
 		playerCamera = GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetAxisRaw("Mouse ScrollWheel")!=0){
-			
+			float scroll = Input.GetAxis ("Mouse ScrollWheel");
+			playerCamera.fieldOfView -= scroll*zoomSpeed;
+			playerCamera.fieldOfView = Mathf.Clamp (playerCamera.fieldOfView,20,100);
 		}
 		transform.position = new Vector3 (cameraTarget.position.x,cameraTarget.position.y+PlayerCameraDistance,cameraTarget.position.z+PlayerCameraDistance);
 	}
